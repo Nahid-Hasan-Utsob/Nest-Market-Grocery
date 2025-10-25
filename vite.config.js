2b84edb62646d473ee55fbc19c2f8dc9269c5d6b
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-10-25 21:36:43 +0600

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // build output folder
+  },
+  server: {
+    port: 5173,
+    open: true,
+    // লোকাল সার্ভারে route reload করলেও 404 না দেয়
+    historyApiFallback: true,
+  },
+  preview: {
+    // vercel এ deploy হবার পরে route reload ঠিক রাখতে সাহায্য করবে
+    historyApiFallback: true,
+  },
 })
